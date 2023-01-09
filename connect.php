1aa0607438d3cfb6f16d8f070706fe5eb3095d8b
Puskarnepal58 <121742965+Puskarnepal58@users.noreply.github.com>
2023-01-09 11:35:00 +0545

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,30 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$checkinDate = $_POST['checkindate'];
+	$checkoutDate = $_POST['checkoutdate'];
+	$rooms = $_POST['rooms'];
+	$numberofpeople = $_POST['number'];
+	$contact = $_POST['contact'];
+
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','mountviewpokhara');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		
+		$stmt = $conn->prepare("insert into booking(name, email, checkinDate, checkoutDate, rooms, numberofpeople, contact) values(?, ?, ?, ?, ?, ?,?)");
+		$stmt->bind_param("ssssssi", $name, $email, $checkinDate, $checkoutDate, $rooms, $numberofpeople, $contact);
+		$execval = $stmt->execute();
+		// echo $execval;
+		echo "Registration completed successfully...<br>Our staff will connect with you shortly.";
+		header('location:thankyou.html');
+		$stmt->close();
+		$conn->close();
+	}
+	
+?>
+  
+  
